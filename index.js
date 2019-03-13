42a9ce3b481a0970427a993e4c42f006d6ff7a1c
trevjnels <trevorjohnnels@gmail.com>
2019-03-13 10:30:13 -0600

    fixed project links to open new tab & added favicon

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const navHandler = function() {
 
 const noPlaceLikeHome = function() {
   $(".left")
-    .html(`<div class="mission-summary"><h2 class="Summary">Summary</h2><p>I am an obsessive and relentless tinkerer and have an unlimited thirst for new information with a strong appreciate for symmetry balance and natural patterns. I read thoroughly too much about the world and like to get caught up in the details. I am a mostly self-taught developer with an enterprise sales background. That said I was destined for engineering as child but was thrown off track. I had all the signs of a young steampunk Jedi: setting up the family computer at 5yo, becoming a mechanical pencil modder in 1st grade, spending much of my childhood solo in my dad’s workshop in the basement, and leading my physics/geometry/math
+    .html(`<div class="mission-summary"><h2 class="Summary">A repressed engineer formerally stuck in a enterprise-sales-person's life</h2><p>I am an obsessive and relentless tinkerer and have an unlimited thirst for new information with a strong appreciate for symmetry balance and natural patterns. I read thoroughly too much about the world and like to get caught up in the details. I am a mostly self-taught developer with an enterprise sales background. That said I was destined for engineering as child but was thrown off track. I had all the signs of a young steampunk Jedi: setting up the family computer at 5yo, becoming a mechanical pencil modder in 1st grade, spending much of my childhood solo in my dad’s workshop in the basement, and leading my physics/geometry/math
     classes without even trying. </p><p>At the ripe age of 12 on a sunny afternoon, I met a mechanical engineer - well a whole bunch of them - in a hot tub in Disney World. They were attending an industry conference. This downtrodden and dissatisfied man told me that engineering was a miserable pursuit and to find greener pastures. At that point I stoped focusing on building. I’ve now seen what its like to do what you do not love for money, Now I'm fixing that and building.
     When not coding, I'm summiting mountains and if(winter){c(snowboarding down them)};.</p></div>`);
   $(".right")
@@ -70,9 +70,9 @@ const projectsRender = function(array) {
       elem.photoSRC
     }'></div><div class="rightProject"><ul class='projectUL'><li class='projectLink'><a href='${
       elem.github
-    }'>Github<a/></li><li class='projectLink'><a href='${
+    }'target='_blank'>Github<a/></li><li class='projectLink'><a href='${
       elem.live
-    }'>Live</a></li></ul></div></div><h2 class='projectDescription'>Description:</h2><p class='projectDescription'> ${
+    }'target='_blank'>Live</a></li></ul></div></div><h2 class='projectDescription'>Description:</h2><p class='projectDescription'> ${
       elem.description
     } </p></div></div>`;
 
--- photoloop.js
+++ photoloop.js
@@ -29,25 +29,25 @@ var projectsArray = [
     photoSRC: "./resources/quizAppPic.png"
   },
   {
-    name: "JqueryQuizApp",
+    name: "Portfolio site",
     description:
-      "This is one of the first top-to-bottom apps I've built using Jquery. It quizes the user on gnarley ski history/facts with a bias tword my home state, Utah. Each question is cycled in randomly from an array and displayed randomly. The user has the option to start over at the end.",
+      "This is the site you are currently viewing. This is the first front-end work I've made that I'm actually proud of. It came after the quiz app and gave me a tough fight. I've gotten confident with backend work and ignored frontend and did not realize how devistatingly frustrating it can get. Some of the highlights of this app is that the app always recenters when you change a page, or resize the window. I've also added a small photo gallary. Both the gallary and borders and randomized everytime the page reloads.",
+    github: "https://github.com/trevjnels/portfolio",
+    live: "https://trevjnels.github.io/portfolio/",
+    photoSRC: "./resources/portfolioAppPic.png"
+  },
+  {
+    name: "DummyWebsite1",
+    description:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     github: "https://github.com/trevjnels/quizAppjq",
     live: "https://trevjnels.github.io/quizAppjq/",
     photoSRC: "./resources/quizAppPic.png"
   },
   {
-    name: "JqueryQuizApp",
+    name: "DummyWebsite2",
     description:
-      "This is one of the first top-to-bottom apps I've built using Jquery. It quizes the user on gnarley ski history/facts with a bias tword my home state, Utah. Each question is cycled in randomly from an array and displayed randomly. The user has the option to start over at the end.",
-    github: "https://github.com/trevjnels/quizAppjq",
-    live: "https://trevjnels.github.io/quizAppjq/",
-    photoSRC: "./resources/quizAppPic.png"
-  },
-  {
-    name: "JqueryQuizApp",
-    description:
-      "This is one of the first top-to-bottom apps I've built using Jquery. It quizes the user on gnarley ski history/facts with a bias tword my home state, Utah. Each question is cycled in randomly from an array and displayed randomly. The user has the option to start over at the end.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     github: "https://github.com/trevjnels/quizAppjq",
     live: "https://trevjnels.github.io/quizAppjq/",
     photoSRC: "./resources/quizAppPic.png"
