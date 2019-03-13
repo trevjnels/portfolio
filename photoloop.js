04272e021314196e560d24f39172055db2184810
trevjnels <trevorjohnnels@gmail.com>
2019-03-13 13:05:46 -0600

    final touches - i hope this works!

--- photoloop.js
+++ photoloop.js
@@ -65,7 +65,8 @@ var projectsArray = [
       "This is one of the first top-to-bottom apps I've built using Jquery. It quizes the user on gnarley ski history/facts with a bias tword my home state, Utah. Each question is cycled in randomly from an array and displayed randomly. The user has the option to start over at the end.",
     github: "https://github.com/trevjnels/quizAppjq",
     live: "https://trevjnels.github.io/quizAppjq/",
-    photoSRC: "./resources/quizAppPic.png"
+    photoSRC: "./resources/quizAppPic.png",
+    skillsUsed: "JQuery, ECMAScript5, CSS, HTML, A11Y"
   },
   {
     name: "Portfolio site",
@@ -73,7 +74,8 @@ var projectsArray = [
       "This is the site you are currently viewing. This is the first front-end work I've made that I'm actually proud of. It came after the quiz app and gave me a tough fight. I've gotten confident with backend work and ignored frontend and did not realize how devistatingly frustrating it can get. Some of the highlights of this app is that the app always recenters when you change a page, or resize the window. I've also added a small photo gallary. Both the gallary and borders and randomized everytime the page reloads.",
     github: "https://github.com/trevjnels/portfolio",
     live: "https://trevjnels.github.io/portfolio/",
-    photoSRC: "./resources/portfolioAppPic.png"
+    photoSRC: "./resources/portfolioAppPic.png",
+    skillsUsed: "JQuery, ECMAScript5, CSS, HTML, A11Y"
   },
   {
     name: "DummyWebsite1",
@@ -81,7 +83,9 @@ var projectsArray = [
       "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     github: "https://github.com/trevjnels/quizAppjq",
     live: "https://trevjnels.github.io/quizAppjq/",
-    photoSRC: "./resources/quizAppPic.png"
+    photoSRC:
+      "https://i.pinimg.com/originals/35/a3/e4/35a3e4e8f8771b6983a992068fa2785b.jpg",
+    skillsUsed: "coding? legos? who knows"
   },
   {
     name: "DummyWebsite2",
@@ -89,7 +93,9 @@ var projectsArray = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     github: "https://github.com/trevjnels/quizAppjq",
     live: "https://trevjnels.github.io/quizAppjq/",
-    photoSRC: "./resources/quizAppPic.png"
+    photoSRC:
+      "https://i.pinimg.com/originals/35/a3/e4/35a3e4e8f8771b6983a992068fa2785b.jpg",
+    skillsUsed: "coding? legos? who knows"
   }
 ];
 
