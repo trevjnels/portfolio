f4ac4669e0807b78efb3ad1c3f28df81776329e0
trevjnels <trevorjohnnels@gmail.com>
2019-03-18 10:44:08 -0600

    removed dummy projects

--- photoloop.js
+++ photoloop.js
@@ -76,26 +76,6 @@ var projectsArray = [
     live: "https://trevjnels.github.io/portfolio/",
     photoSRC: "./resources/portfolioAppPic.png",
     skillsUsed: "JQuery, ECMAScript5, CSS, HTML, A11Y"
-  },
-  {
-    name: "DummyWebsite1",
-    description:
-      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    github: "https://github.com/trevjnels/quizAppjq",
-    live: "https://trevjnels.github.io/quizAppjq/",
-    photoSRC:
-      "https://i.pinimg.com/originals/35/a3/e4/35a3e4e8f8771b6983a992068fa2785b.jpg",
-    skillsUsed: "coding? legos? who knows"
-  },
-  {
-    name: "DummyWebsite2",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    github: "https://github.com/trevjnels/quizAppjq",
-    live: "https://trevjnels.github.io/quizAppjq/",
-    photoSRC:
-      "https://i.pinimg.com/originals/35/a3/e4/35a3e4e8f8771b6983a992068fa2785b.jpg",
-    skillsUsed: "coding? legos? who knows"
   }
 ];
 
